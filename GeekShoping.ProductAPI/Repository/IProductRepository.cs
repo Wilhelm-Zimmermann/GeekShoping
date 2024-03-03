33dcf740a6dbcf3a3073753939178dec4a3144b0
Wilhelm Zimmermann <wilhelmzim4@outlook.com>
2024-03-03 09:02:54 -0300

    microsservice ready

--- GeekShoping.ProductAPI/Repository/IProductRepository.cs
+++ GeekShoping.ProductAPI/Repository/IProductRepository.cs
@@ -1,7 +1,14 @@
-﻿namespace GeekShoping.ProductAPI.Repository
+﻿using GeekShoping.ProductAPI.Data.ValueObjects;
+
+namespace GeekShoping.ProductAPI.Repository
 {
     public interface IProductRepository
     {
+        Task<IEnumerable<ProductVO>> FindAll();
+        Task<ProductVO> FindById(long id);
 
+        Task<ProductVO> Create(ProductVO product);
+        Task<ProductVO> Update(ProductVO product);
+        Task<bool> DeleteById(long id);
     }
 }
